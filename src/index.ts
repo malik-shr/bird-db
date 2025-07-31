7cd2f8b8c97035b61ea2ebf81d013a15aea67352
malik-shr <malik.skw@proton.me>
2025-07-31 22:35:02 +0200

    export sql functions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import { QueryBuilder } from './queryBuilder/QueryBuilder';
 import { or, and, ref, whereRef, c } from './helpers/WhereHelpers';
+import {
+  COUNT,
+  SUM,
+  MIN,
+  MAX,
+  AVG,
+  UPPER,
+  LOWER,
+  LENGTH,
+} from './helpers/sqlFunctions';
 
 export { QueryBuilder };
 export { or, and, ref, whereRef, c };
+export { COUNT, SUM, MIN, MAX, AVG, UPPER, LOWER, LENGTH };
